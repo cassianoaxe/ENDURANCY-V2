0cd4374269ea23a39c0657f8289262678397828c
cassianoaxe <36527019-cassianoaxe@users.noreply.replit.com>
2025-04-18 20:21:07 +0000

    Restored to '9f0dfe8a4e28ee253cbadcf7494add55a88f0ae2'
    Replit-Restored-To: 9f0dfe8a4e28ee253cbadcf7494add55a88f0ae2

--- shared/schema-combined.ts
+++ shared/schema-combined.ts
@@ -28,9 +28,6 @@ import * as patrimonioSchema from './schema-patrimonio';
 // Importando o schema de pesquisa científica
 import * as pesquisaSchema from './schema-pesquisa';
 
-// Importando o schema de laboratório
-import * as laboratorioSchema from './schema-laboratorio';
-
 // Temporariamente removido devido a erros
 // import * as financeiroSchema from './schema-financeiro';
 
@@ -44,8 +41,7 @@ export const combinedSchema = {
   ...tarefasSchema,
   ...transparenciaSchema,
   ...patrimonioSchema,
-  ...pesquisaSchema,
-  ...laboratorioSchema
+  ...pesquisaSchema
 };
 
 // Exportando todos os tipos e enums
@@ -59,4 +55,3 @@ export * from './schema-tarefas';
 export * from './schema-transparencia';
 export * from './schema-patrimonio';
 export * from './schema-pesquisa';
-export * from './schema-laboratorio';
